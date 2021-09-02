c34fce1c8f9a941b14dadb3b55f0e607349d6561
rojasj12 <89586709+rojasj12@users.noreply.github.com>
2021-09-02 09:52:31 -0400

    Add files via upload

---
+++ HW1_Joel Rojas.R
@@ -0,0 +1,12 @@
+getwd()
+load("acs2017_ny_data.RData")
+acs2017_ny[1:10,1:7]
+attach(acs2017_ny)
+summary(acs2017_ny)
+print(NN_obs <- length(AGE))
+summary(AGE[female == 1])
+summary(AGE[!female])
+mean(AGE[female == 1])
+sd(AGE[female == 1])
+mean(AGE[!female])
+sd(AGE[!female])
